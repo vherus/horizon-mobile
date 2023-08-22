c6d70d38da06949445faa2498825348f7da6e694
Nathan King <nathanking3590@gmail.com>
2023-08-22 15:02:57 +0100

    it all began with a stone

---
+++ App.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler'
+import Navigation from './src/Navigation'
+
+export default function App() {
+    return <Navigation />
+}
---
+++ src/Navigation.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './screens/Home'
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Home/>
+        </NavigationContainer>
+    )
+}
