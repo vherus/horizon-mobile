b673afdc1734102224bf4deac731ec4d0f43663a
Nathan King <nathanking3590@gmail.com>
2023-08-24 20:57:36 +0100

    add seacom type map

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,91 @@ export const STYLES = {
     primaryColour: '#50b4d6',
     secondaryColour: '#374450'
 }
+
+export const SEACOM_TYPES = {
+    '17': {
+        type: 'EXP Party',
+        category: 'Seek Party',
+        color: '#1f8a36',
+    },
+    '18': {
+        type: 'EXP Party',
+        category: 'Find Member',
+        color: '#1f8a36',
+    },
+    '19': {
+        type: 'EXP Party',
+        category: 'Other',
+        color: '#1f8a36',
+    },
+    '33': {
+        type: 'Battle Content',
+        category: 'Seek Party',
+        color: '#c9c6b3',
+    },
+    '34': {
+        type: 'Battle Content',
+        category: 'Find Member',
+        color: '#c9c6b3',
+    },
+    '35': {
+        type: 'Battle Content',
+        category: 'Other',
+        color: '#c9c6b3',
+    },
+    '49': {
+        type: 'Missions & Quests',
+        category: 'Seek Party',
+        color: '#ad241a',
+    },
+    '50': {
+        type: 'Missions & Quests',
+        category: 'Find Member',
+        color: '#ad241a',
+    },
+    '51': {
+        type: 'Missions & Quests',
+        category: 'Other',
+        color: '#ad241a',
+    },
+    '65': {
+        type: 'Item',
+        category: 'Want to Sell',
+        color: '#5653a4',
+    },
+    '66': {
+        type: 'Item',
+        category: 'Want to Buy',
+        color: '#5653a4',
+    },
+    '67': {
+        type: 'Item',
+        category: 'Other',
+        color: '#5653a4',
+    },
+    '81': {
+        type: 'Linkshell',
+        category: 'Looking for LS',
+        color: '#b976bc',
+    },
+    '82': {
+        type: 'Linkshell',
+        category: 'Recruiting',
+        color: '#b976bc',
+    },
+    '83': {
+        type: 'Linkshell',
+        category: 'Other',
+        color: '#b976bc',
+    },
+    '97': {
+        type: 'Looking for Friends',
+        category: null,
+        color: '#4a3a34',
+    },
+    '115': {
+        type: 'Others',
+        category: null,
+        color: '#bdba1f',
+    },
+};
